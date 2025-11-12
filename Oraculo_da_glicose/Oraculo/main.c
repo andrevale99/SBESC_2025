6c822d90f0a19cd2215c2cc8b5270293f0f7c3bc
andrevale99 <andrefvale@gmail.com>
2025-11-12 14:36:11 -0300

    NTP e organização

--- Oraculo_da_glicose/Oraculo/main.c
+++ Oraculo_da_glicose/Oraculo/main.c
@@ -56,76 +56,8 @@ int main()
     BLE_Init(&handleQueue_to_Mqtt);
 
     static MQTT_CLIENT_DATA_T state;
-    // Use board unique id
-    char unique_id_buf[5];
-    pico_get_unique_board_id_string(unique_id_buf, sizeof(unique_id_buf));
-    for (int i = 0; i < sizeof(unique_id_buf) - 1; i++)
-    {
-        unique_id_buf[i] = tolower(unique_id_buf[i]);
-    }
 
-    // Generate a unique name, e.g. pico1234
-    char client_id_buf[sizeof(MQTT_DEVICE_NAME) + sizeof(unique_id_buf) - 1];
-    memcpy(&client_id_buf[0], MQTT_DEVICE_NAME, sizeof(MQTT_DEVICE_NAME) - 1);
-    memcpy(&client_id_buf[sizeof(MQTT_DEVICE_NAME) - 1], unique_id_buf, sizeof(unique_id_buf) - 1);
-    client_id_buf[sizeof(client_id_buf) - 1] = 0;
-    printf("Device name %s\n", client_id_buf);
-
-    state.mqtt_client_info.client_id = client_id_buf;
-    state.mqtt_client_info.keep_alive = MQTT_KEEP_ALIVE_S; // Keep alive in sec
-#if defined(MQTT_USERNAME) && defined(MQTT_PASSWORD)
-    state.mqtt_client_info.client_user = MQTT_USERNAME;
-    state.mqtt_client_info.client_pass = MQTT_PASSWORD;
-#else
-    state.mqtt_client_info.client_user = NULL;
-    state.mqtt_client_info.client_pass = NULL;
-#endif
-    static char will_topic[MQTT_TOPIC_LEN];
-    strncpy(will_topic, full_topic(&state, MQTT_WILL_TOPIC), sizeof(will_topic));
-    state.mqtt_client_info.will_topic = will_topic;
-    state.mqtt_client_info.will_msg = MQTT_WILL_MSG;
-    state.mqtt_client_info.will_qos = MQTT_WILL_QOS;
-    state.mqtt_client_info.will_retain = true;
-#if LWIP_ALTCP && LWIP_ALTCP_TLS
-    // TLS enabled
-#ifdef MQTT_CERT_INC
-    static const uint8_t ca_cert[] = TLS_ROOT_CERT;
-    static const uint8_t client_key[] = TLS_CLIENT_KEY;
-    static const uint8_t client_cert[] = TLS_CLIENT_CERT;
-    // This confirms the indentity of the server and the client
-    state.mqtt_client_info.tls_config = altcp_tls_create_config_client_2wayauth(ca_cert, sizeof(ca_cert),
-                                                                                client_key, sizeof(client_key), NULL, 0, client_cert, sizeof(client_cert));
-#if ALTCP_MBEDTLS_AUTHMODE != MBEDTLS_SSL_VERIFY_REQUIRED
-    WARN_printf("Warning: tls without verification is insecure\n");
-#endif
-#else
-    state->client_info.tls_config = altcp_tls_create_config_client(NULL, 0);
-    WARN_printf("Warning: tls without a certificate is insecure\n");
-#endif
-
-#endif
-
-    cyw43_arch_enable_sta_mode();
-    if (cyw43_arch_wifi_connect_timeout_ms(WIFI_SSID, WIFI_PASSWORD, CYW43_AUTH_WPA2_AES_PSK, 30000))
-        printf("Failed to connect");
-    else
-        printf("\nConnected to Wifi\n");
-
-    // We are not in a callback so locking is needed when calling lwip
-    // Make a DNS request for the MQTT server IP address
-    cyw43_arch_lwip_begin();
-    int err = dns_gethostbyname(MQTT_SERVER, &state.mqtt_server_address, dns_found, &state);
-    cyw43_arch_lwip_end();
-
-    if (err == ERR_OK)
-    {
-        // We have the address, just start the client
-        start_client(&state);
-    }
-    else if (err != ERR_INPROGRESS)
-    { // ERR_INPROGRESS means expect a callback
-        panic("dns request failed");
-    }
+    Mqtt_setup(&state);
 
     xTaskCreate(vTaskMqtt, "MQTT Task", 2048, (void *)&state, 1, NULL);
 
